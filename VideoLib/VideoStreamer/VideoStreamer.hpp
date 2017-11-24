a48bf1ea0bc98bf53c4f87018f53d8f8675715f6
Daniel Loman <dloman@toyon.com>
2017-11-24 11:49:51 -0800

    added streamer (needs lots of cleanup)

---
+++ VideoLib/VideoStreamer/VideoStreamer.hpp
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <string>
+
+class AVBitStreamFilterContext;
+class AVCodecContext;
+class AVFrame;
+class AVFormatContext;
+class AVOutputFormat;
+class AVStream;
+
+namespace vl
+{
+  class VideoStreamer
+  {
+    public:
+
+      VideoStreamer(const std::string& Url, int Width, int Height);
+
+      ~VideoStreamer();
+
+      void StreamFrame(AVFrame* pFrame);
+
+    private:
+
+      void SetCodec();
+
+      AVFormatContext* mpOutputFormatContext;
+
+      AVCodecContext* mpCodecContext;
+
+      AVStream* mpStream;
+
+      size_t mFrameIndex;
+
+  };
+}
